2e6317b8fece244df984a55500d5def5c7cfac6d
JayArrowz <arrowzflame321@gmail.com>
2025-03-20 18:50:29 +0000

    feat: wip

--- dist/src/api/ApiBase.d.ts
+++
@@ -1,6 +0,0 @@
-import { Api } from "./Api";
-import { QueryClient } from "react-query";
-declare const queryClient: QueryClient;
-declare const apiClientInstance: Api<unknown>;
-export default apiClientInstance;
-export { queryClient };
